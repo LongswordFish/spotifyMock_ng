99ae360d7013dfdbf6077e636a31eb1e6c3a5753
longswordfish <robinyu9840@gmail.com>
2022-02-03 11:32:54 -0500

    minor fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,4 @@ export const environment = {
   clientID: "c4cdcb0c82664dacadb0b205667137f0",
   clientSecret: "fe615600063148ea91a041704b693da4",
    userAPIBase: "https://fishapi-for-web422.herokuapp.com/api/user"
-  //userAPIBase: "https://web422-api-a6.herokuapp.com/api/user"
 };
